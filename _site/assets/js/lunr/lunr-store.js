cf59041a437cdfc071e9fac89a1f0455dff2d972
Bikram Dhoju <bikram@localhost.localdomain>
2018-09-06 00:21:11 +0545

    google analytics

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,25 +2,25 @@ var store = [{
         "title": "Let’s boost the web:Does speed of your website really matter?",
         "excerpt":"It’s a no-brainer: slow web performance torments every internet user. Before I dig deep into unveiling the bottlenecks of your web performance, it’s really important to estimate the cost of your performance, and there is no better way to articulate the cost of performance better than the old story of...","categories": ["web"],
         "tags": ["Web Developemt","Javascript"],
-        "url": "http://0.0.0.0:4000/web/Lets-Boost-the-web/",
-        "teaser":"http://0.0.0.0:4000/assets/images/main.jpg"},{
+        "url": "http://localhost:4000/web/Lets-Boost-the-web/",
+        "teaser":"http://localhost:4000/assets/images/main.jpg"},{
         "title": "Intrusion Dectection system with Fail2ban",
         "excerpt":"Install and configure fail2ban to stop some hacking/DOS (Denial of Service) attempts on Pustakalaya Server. As this site’s been hit by the occasional DOS from people with an axe to grind and too much time on their hands,fail2ban is lifesaver. Please follow the steps below: Step 1: Installation Install fail2ban...","categories": ["Network&System"],
         "tags": ["security"],
-        "url": "http://0.0.0.0:4000/network&system/Intrusion-Detection-System-with-Fail2Ban/",
-        "teaser":"http://0.0.0.0:4000/assets/images/IDS/IDS.png"},{
+        "url": "http://localhost:4000/network&system/Intrusion-Detection-System-with-Fail2Ban/",
+        "teaser":"http://localhost:4000/assets/images/IDS/IDS.png"},{
         "title": "Bringing Raspberry Pi to Classroom",
         "excerpt":"There is so much going on in technology these days. Technology has brought unprecedented changes in our daily life, retooling the way we communicate, the way we shop, the way we make our living and more. The things that were considered as science-fiction a few years ago, is now a...","categories": ["Edtech"],
         "tags": ["RaspberryPi"],
-        "url": "http://0.0.0.0:4000/edtech/Bringing-RPi-to-Classroom/",
-        "teaser":"http://0.0.0.0:4000/assets/images/main.jpg"},{
+        "url": "http://localhost:4000/edtech/Bringing-RPi-to-Classroom/",
+        "teaser":"http://localhost:4000/assets/images/main.jpg"},{
         "title": "DNS :Introduction (DNS Series:Part I)",
         "excerpt":"There are many servers all over the world. Every server has a unique IP Address and is identified by that IP Address. That brings us to the bulk of the IP address. And you the pproblem with numbers they hard to remember due to their intangibility, the human has always...","categories": ["Network&System"],
         "tags": ["Internet","DNS","Javascript"],
-        "url": "http://0.0.0.0:4000/network&system/DNS-Series-1/",
-        "teaser":"http://0.0.0.0:4000/assets/images/DNS/DNS.png"},{
+        "url": "http://localhost:4000/network&system/DNS-Series-1/",
+        "teaser":"http://localhost:4000/assets/images/DNS/DNS.png"},{
         "title": "Setting up Private DNS Server with BIND9  (DNS Series:Part II)",
         "excerpt":"We already dived into the nitty gritty of the theory about what exactly it is, how it works and what are its parts you can read the part one here. In this series, I will be talking about how to set up a private DNS server using BIND9. Before that,...","categories": ["Network&System"],
         "tags": ["Internet","DNS","Javascript"],
-        "url": "http://0.0.0.0:4000/network&system/DNS-Series-2/",
-        "teaser":"http://0.0.0.0:4000/assets/images/DNS/DNS.png"}]
+        "url": "http://localhost:4000/network&system/DNS-Series-2/",
+        "teaser":"http://localhost:4000/assets/images/DNS/DNS.png"}]
